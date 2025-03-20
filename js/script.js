aefe9b073466beabe2c1874159ef50e09dce9e8c
Joao Perondi <200586154@student.georgianc.on.ca>
2025-03-20 15:19:14 -0400

    Create the class and the constructor

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+//Create the Smoothie class
+class Smoothie {
+    //Constructor to initilise the properties
+    constructor(size, ingredients, base) {
+        this.size = size; 
+        this.ingredients = ingredients; 
+        this.base = base;
+    }
+}
